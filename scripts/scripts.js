e52b156c47eec11e1fbfdd6d23eaed941cb92396
stellhom <estellahom@gmail.com>
2016-07-15 00:45:16 -0400

    Added ANIMATION to the hamburger menu!!

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,13 +1,45 @@
-// mobile navigation toggle function
+
+// mobile navigation hamburger menu animation
+// $( ".nav-toggle" ).click(function() {
+//     $(this).toggleClass("open");
+//     $("nav").fadeToggle(100);
+//
+//     return false;
+// });
+
 
 $(function() {
   $('body').addClass('js');
   var $menu = $('#menu'),
-      $menulink = $('.menu-link');
+      $menulink = $('.nav-toggle');
 
   $menulink.click(function() {
+    $(this).toggleClass("open");
     $menulink.toggleClass('active');
     $menu.toggleClass('active');
+    // $("nav").fadeToggle(100);
+
     return false;
   });
 });
+
+
+
+
+
+
+
+
+// mobile navigation toggle function
+
+// $(function() {
+//   $('body').addClass('js');
+//   var $menu = $('#menu'),
+//       $menulink = $('.menu-link');
+//
+//   $menulink.click(function() {
+//     $menulink.toggleClass('active');
+//     $menu.toggleClass('active');
+//     return false;
+//   });
+// });
