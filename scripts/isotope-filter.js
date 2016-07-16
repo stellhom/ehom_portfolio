68680c7295b3e79ea5462d05121c77e329b34a94
stellhom <estellahom@gmail.com>
2016-07-16 17:57:50 -0400

    YES!!! Got my isotope filter working on the masonry grid!!
    Added in jquery with document.ready function, added in css styles,
    applied style from previous js buttons to the links.

---
+++ scripts/isotope-filter.js
@@ -0,0 +1,28 @@
+// Used Isotope plugin for filtering projects
+$(document).ready(function() {
+    var $container = $('#proj-grid');
+    $container.isotope({
+        filter: '*',
+        animationOptions: {
+            duration: 750,
+            easing: 'linear',
+            queue: false
+        }
+    });
+
+    $('#filters a').click(function(){
+        $('#filters .current').removeClass('current');
+        $(this).addClass('current');
+
+        var selector = $(this).attr('data-filter');
+        $container.isotope({
+            filter: selector,
+            animationOptions: {
+                duration: 750,
+                easing: 'linear',
+                queue: false
+            }
+         });
+         return false;
+    });
+});
--- scripts/masonry-grid.js
+++ scripts/masonry-grid.js
@@ -1,15 +1,32 @@
-// use Masonry as a jQuery plugin 
+// jQuery to set up Masonry using document.ready function
 
-var $container = jQuery('#proj-grid');   //first set main container as variable
-$container.masonry({   // initialize -- apply masonry effect to container
-  // columnWidth: 200,
-  // wasn't working so removed and let media queries handle the resizing in percentages (responsive)
-  itemSelector: '.proj-width'   //targets each image in grid -- use css & media queries for sizing
+$(document).ready(function() {
+$('#proj-grid').masonry({
+// columnWidth: 320,
+itemSelector: '.proj-width'
+}).imagesLoaded(function() {
+$('#proj-grid').masonry('reload');
+});
 });
 
+// OLD SCRIPTS
+
+// jQuery to set up Masonry
+//first set main container as variable
+// var $container = jQuery('#proj-grid');
+
+// initialize -- apply masonry effect to container
+// $container.masonry({
+
+   // columnWidth: 200,
+// wasn't working so removed and let media queries handle the resizing in percentages (responsive)
+
+//targets each image in grid -- use css & media queries for sizing
+//   itemSelector: '.proj-width'
+// });
 
 
-// Vanilla Javascript version (works too!)
+// or can use Vanilla Javascript version (works too!)
 // var container = document.querySelector('#proj-grid');
 // var msnry = new Masonry( container, {
 //   itemSelector: '.proj-width'
